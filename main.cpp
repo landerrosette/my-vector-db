ebe5b0525aa44134682d6a63c487145883e29ced
landerrosette <57791410+landerrosette@users.noreply.github.com>
2025-04-08 12:55:28 +0000

    Update CMakeLists.txt and main.cpp to support new components and logging

--- main.cpp
+++ main.cpp
@@ -1,8 +1,19 @@
-#include <iostream>
-#include "FaissIndex.h"
+#include "HttpServer.h"
+#include "IndexFactory.h"
+#include "logger.h"
 
 int main() {
-    FaissIndex index(nullptr);
+    init_global_logger();
+    GlobalLogger->info("Global logger initialized");
+
+    int dim = 1;
+    IndexFactory *globalIndexFactory = getGlobalIndexFactory();
+    globalIndexFactory->init(IndexFactory::IndexType::FLAT, dim);
+    GlobalLogger->info("Global index factory initialized");
+
+    HttpServer server("0.0.0.0", 8080);
+    GlobalLogger->info("HTTP server created");
+    server.start();
 
     return 0;
 }
