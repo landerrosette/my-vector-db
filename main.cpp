e45a55e48ebbb25d6201fa4aed3d8a0c81b09d42
landerrosette <57791410+landerrosette@users.noreply.github.com>
2025-04-17 12:16:13 +0000

    Add HNSW index initialization in main and include HNSWLibIndex in CMake

--- main.cpp
+++ main.cpp
@@ -7,9 +7,11 @@ int main() {
     set_log_level(spdlog::level::debug);
     GlobalLogger->info("Global logger initialized");
 
-    int dim = 1;
+    int dim = 1; // Dimension of the vectors
+    int num_data = 1000; // Number of data points
     IndexFactory *globalIndexFactory = getGlobalIndexFactory();
     globalIndexFactory->init(IndexFactory::IndexType::FLAT, dim);
+    globalIndexFactory->init(IndexFactory::IndexType::HNSW, dim, num_data);
     GlobalLogger->info("Global index factory initialized");
 
     HttpServer server("0.0.0.0", 8080);
