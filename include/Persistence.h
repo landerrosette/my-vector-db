d7383826b3673534e858e1a36a2e474601f9ed54
landerrosette <57791410+landerrosette@users.noreply.github.com>
2025-04-26 20:31:30 +0000

    feat: Enhance Persistence class with snapshot management

--- include/Persistence.h
+++ include/Persistence.h
@@ -6,6 +6,7 @@
 #include <string>
 #include <utility>
 
+#include "ScalarStorage.h"
 #include "rapidjson/document.h"
 
 class Persistence {
@@ -14,7 +15,7 @@ public:
 
     ~Persistence() { if (wal_log_file.is_open()) wal_log_file.close(); }
 
-    void init(const std::string &local_path);
+    void init(const std::string &local_path, ScalarStorage &scalar_storage);
 
     uint32_t increase_id() { return ++id; }
 
@@ -25,9 +26,18 @@ public:
 
     std::pair<std::string, rapidjson::Document> read_next_wal_log();
 
+    void save_last_snapshot_id(ScalarStorage &scalar_storage);
+
+    void load_last_snapshot_id(ScalarStorage &scalar_storage);
+
+    void take_snapshot(ScalarStorage &scalar_storage);
+
+    void load_snapshot(ScalarStorage &scalar_storage);
+
 private:
     uint32_t id = 0;
     std::fstream wal_log_file;
+    uint32_t last_snapshot_id = 0;
 };
 
 
--- src/Persistence.cpp
+++ src/Persistence.cpp
@@ -3,16 +3,18 @@
 #include <sstream>
 #include <stdexcept>
 
+#include "IndexFactory.h"
 #include "logger.h"
 #include "rapidjson/stringbuffer.h"
 #include "rapidjson/writer.h"
 
-void Persistence::init(const std::string &local_path) {
+void Persistence::init(const std::string &local_path, ScalarStorage &scalar_storage) {
     wal_log_file.open(local_path, std::ios::in | std::ios::out | std::ios::app);
     if (!wal_log_file.is_open()) {
         global_logger->error("Error opening WAL log file: {}", std::strerror(errno));
         throw std::runtime_error("Error opening WAL log file: " + std::string(std::strerror(errno)));
     }
+    load_last_snapshot_id(scalar_storage);
 }
 
 void Persistence::write_wal_log(const std::string &operation_type, const rapidjson::Document &json_data,
@@ -32,7 +34,7 @@ void Persistence::write_wal_log(const std::string &operation_type, const rapidjs
 
 std::pair<std::string, rapidjson::Document> Persistence::read_next_wal_log() {
     global_logger->debug("Reading next WAL log entry");
-    if (std::string line; std::getline(wal_log_file, line)) {
+    for (std::string line; std::getline(wal_log_file, line);) {
         std::istringstream iss(line);
         std::string log_id_str, version, operation_type, json_data_str;
         std::getline(iss, log_id_str, '|');
@@ -43,17 +45,43 @@ std::pair<std::string, rapidjson::Document> Persistence::read_next_wal_log() {
         uint32_t log_id = std::stoul(log_id_str);
         if (log_id > id) id = log_id;
 
-        rapidjson::Document json_data;
-        json_data.Parse(json_data_str.c_str());
-        rapidjson::StringBuffer buffer;
-        rapidjson::Writer writer(buffer);
-        json_data.Accept(writer);
-        global_logger->debug("Read WAL log entry: {}|{}|{}|{}", log_id_str, version, operation_type,
-                             buffer.GetString());
-        return {std::move(operation_type), std::move(json_data)};
-    } else {
-        wal_log_file.clear();
-        global_logger->debug("No more WAL log entries to read");
-        return {};
+        if (log_id > last_snapshot_id) {
+            rapidjson::Document json_data;
+            json_data.Parse(json_data_str.c_str());
+            rapidjson::StringBuffer buffer;
+            rapidjson::Writer writer(buffer);
+            json_data.Accept(writer);
+            global_logger->debug("Read WAL log entry: {}|{}|{}|{}", log_id_str, version, operation_type,
+                                 buffer.GetString());
+            return {std::move(operation_type), std::move(json_data)};
+        } else
+            global_logger->debug("Skipped WAL log entry: {}|{}|{}|{}", log_id_str, version, operation_type,
+                                 json_data_str);
     }
+    wal_log_file.clear();
+    global_logger->debug("No more WAL log entries to read");
+    return {};
+}
+
+void Persistence::save_last_snapshot_id(ScalarStorage &scalar_storage) {
+    scalar_storage.put("snapshots_max_log_id", std::to_string(last_snapshot_id));
+    global_logger->debug("Saved last snapshot ID: {}", last_snapshot_id);
+}
+
+void Persistence::load_last_snapshot_id(ScalarStorage &scalar_storage) {
+    std::string last_snapshot_id_str = scalar_storage.get("snapshots_max_log_id");
+    last_snapshot_id = std::stoul(last_snapshot_id_str);
+    global_logger->debug("Loaded last snapshot ID: {}", last_snapshot_id);
+}
+
+void Persistence::take_snapshot(ScalarStorage &scalar_storage) {
+    global_logger->debug("Taking snapshot");
+    last_snapshot_id = id;
+    get_global_index_factory().save_index("snapshots_", scalar_storage);
+    save_last_snapshot_id(scalar_storage);
+}
+
+void Persistence::load_snapshot(ScalarStorage &scalar_storage) {
+    global_logger->debug("Loading snapshot");
+    get_global_index_factory().load_index("snapshots_", scalar_storage);
 }
